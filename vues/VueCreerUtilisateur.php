16afb6767a607cb804db7e8684495dedbdaa15d9
esteve.n <esteve.n@I12-20.sio.local>
2015-11-03 15:18:20 +0100

    creation de la vue pour creer utilisateur

---
+++ vues/VueCreerUtilisateur.php
@@ -0,0 +1,15 @@
+<?php
+	// Projet Réservations M2L - version web mobile
+	// Fonction de la vue VueConnecter.php : visualiser la vue de connexion
+	// Ecrit le 12/10/2015 par Jim
+?>
+<!doctype html>
+<html>
+	<head>	
+		<?php include_once ('vues/head.php'); ?>
+	</head> 
+	<body>
+		<div data-role="page">
+			<div data-role="header" data-theme="<?php echo $themeNormal; ?>">
+				<h4>M2L-GRR</h4>
+			</div>
