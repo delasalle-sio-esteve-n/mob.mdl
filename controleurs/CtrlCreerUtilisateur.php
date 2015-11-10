7a5099fade671452a4e9e6170af0b6cc5a1e391c
esteve.n <esteve.n@I12-20.sio.local>
2015-11-10 13:48:19 +0100

    commitcontroleur+vue creerUtilisateur

---
+++ controleurs/CtrlCreerUtilisateur.php
@@ -0,0 +1,8 @@
+<?php
+if ( ! isset ($_POST ["nom"]) == true || $POST["mdp"]== true||$POST["choix"]== true ){
+	
+	
+	
+	
+	
+}
--- vues/VueCreerUtilisateur.php
+++ vues/VueCreerUtilisateur.php
@@ -14,15 +14,15 @@
 				<h4>M2L-GRR</h4>
 			</div>
 			
-			<form>
+			<form name="form1" id="form1" action="index.php?action=CreerUtilisateur" method="post">
 			<label for="nom">Utilisateur :</label>
 			<input type="text" name="nom" id="nom" data-mini="true" placeholder="Entrez votre nom d'utilisateur" value="<?php echo $nom; ?>" >
 			<label for="mdp">Mot de passe :</label>
 			<input type="<?php if ($afficherMdp == 'off') echo 'password'; else echo 'text'; ?>" name="mdp" id="mdp" data-mini="true" placeholder="Entrez votre mot de passe" value="<?php echo $mdp; ?>" >
-			<ul data-role="listview" data-inset="true">>
-					<li><a href="">Invité</a></li>
-					<li><a href="">Utilisateur</a></li>
-					<li><a href="">Administrateur</a></li>
+			
+			<input type="checkbox" name="choix" value="1"> Invite<br>
+	    	<input type="checkbox" name="choix" value="2"> Utilisateur<br>
+	    	<input type="checkbox" name="choix" value="3"> Administrateur<br>
 			</ul>
 			
 			</form>
@@ -33,6 +33,6 @@
 			<div data-role="footer" data-position="fixed" data-theme="<?php echo $themeNormal; ?>">
 				<h4>Menu</h4>
 			</div>
-		</div>
+		
 	</body>
 </html>
