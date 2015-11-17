8323bd3ab4ed499a0971b22776ae5a1a7a012121
esteve.n <esteve.n@I12-20.sio.local>
2015-11-17 16:11:24 +0100

    finalisation solution 1 Création utilisateur

--- controleurs/CtrlCreerUtilisateur.php
+++ controleurs/CtrlCreerUtilisateur.php
@@ -14,7 +14,7 @@ else
 	// récupération des données postées
 	if ( empty ($_POST ["mail"]) == true)  $mail = "";  else   $mail = $_POST ["mail"];
 	if ( empty ($_POST ["nom"]) == true)  $nom = "";  else   $nom = $_POST ["nom"];
-	if ( empty ($_POST ["choix"]) == true)  $nom = "";  else   $choix = $_POST ["choix"];
+	if ( empty ($_POST ["choix"]) == true)  $choix = "";  else   $choix = $_POST ["choix"];
 	
 	if ($mail == '' || $nom  == "") {
 		// si les données sont incomplètes, réaffichage de la vue avec un message explicatif
@@ -25,8 +25,8 @@ else
 	else 
 	{
 				// connexion du serveur web à la base MySQL ("include_once" peut être remplacé par "require_once")
-				include_once ('../modele/DAO.class.php');
-				include_once ('../modele/Outils.class.php');
+				include_once ('/modele/DAO.class.php');
+				include_once ('/modele/Outils.class.php');
 				$dao = new DAO();
 
 					if ( $dao->existeUtilisateur($nom) )
@@ -41,24 +41,24 @@ else
 					
 						$mdp = Outils::creermdp();
 						// enregistre l'utilisateur dans la bdd
-						$ok = $dao->enregistrerUtilisateur($nom, $choix, $mdp, $email);
+						$ok = $dao->enregistrerUtilisateur($nom, $choix, $mdp, $mail);
 						if ( ! $ok )
+						{
+							$msgFooter = 'problème lors de l\'enregistrement du nouveau utilisateur.';
+							$themeFooter = $themeProbleme;
+							include_once ('vues/VueCreerUtilisateur.php');
 							TraitementAnormal("Erreur : problème lors de l'enregistrement du nouveau utilisateur.");
+						}
 						else
-							TraitementNormal();
-					}
-				}
-				// ferme la connexion à MySQL :
-				unset($dao);
-				
-
-			// connexion du serveur web à la base MySQL
-				include_once ('../modele/Outils.class.php');
-			// envoie un mail à l'utilisateur avec son mot de passe 
-			$message = "Votre mot de passe d'accès au service Réservations M2L a été Crée.\n\n";
-			$message = $message.$Mdp;
-			$ok =Outils::envoyerMail($mail,'Mot de passe M2L', $message, 'delasalle.sio.esteve.n@gmail.com');
-			if($ok)
+						{
+						unset($dao);
+					// connexion du serveur web à la base MySQL
+						include_once ('../modele/Outils.class.php');
+					// envoie un mail à l'utilisateur avec son mot de passe 
+					$message = "Votre mot de passe d'accès au service Réservations M2L a été Crée.\n\n";
+					$message = $message.$Mdp;
+					$ok =Outils::envoyerMail($mail,'Mot de passe M2L', $message, 'delasalle.sio.esteve.n@gmail.com');
+					if($ok)
 					{
 						
 						$msgFooter = 'Enregistrement effectué.<br>L\'envoi du mail de confirmation a rencontré un problème. ';
@@ -71,8 +71,15 @@ else
 						$msgFooter = "Enregistrement effectué.<br>Vous allez recevoir un mail de confirmation contenant votre mot de passe.";
 						include_once ('vues/VueCreerUtilisateur.php');
 					}
-					unset($dao);		// fermeture de la connexion à MySQL
+				}
+			}
 		}
+	}
+
+				
+				
+		
+
 		
 	
 
