adfcc3622bdc81d548c8aa349662bfb7c280d59f
perrinia.s <perrinia.s@I12-19.sio.local>
2015-11-17 14:45:50 +0100

    Solution confirmerReservation

--- controleurs/CtrlConfirmerReservation.php
+++ controleurs/CtrlConfirmerReservation.php
@@ -56,7 +56,7 @@ else
 				{
 					$msgFooter = "Cette réservation est déjà confirmée !";
 					$themeFooter = $themeProbleme;
-					include_once ('vues/VueConfirmerReservation.php');
+					include_once ('vues/VueConfirmerReservation.phpReservation.php');
 				}
 				else
 				{
